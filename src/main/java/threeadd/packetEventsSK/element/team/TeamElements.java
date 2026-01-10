caf103bd6e39d59c1884865de82df7a85622f87e
jasper <jasper.aerden@icloud.com>
2026-01-10 18:29:37 +0100

    Initial Release

--- src/main/java/threeadd/packetEventsSK/element/team/TeamElements.java
+++ src/main/java/threeadd/packetEventsSK/element/team/TeamElements.java
@@ -3,7 +3,6 @@ package threeadd.packetEventsSK.element.team;
 import threeadd.packetEventsSK.util.registry.element.ElementCollection;
 
 public class TeamElements implements ElementCollection {
-
     @Override
     public String identifier() {
         return "team";
