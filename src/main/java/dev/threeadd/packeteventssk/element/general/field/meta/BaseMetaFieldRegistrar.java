3d08a658e7fa45b9a8086c3aa34a4f780c8e7664
3add <3add.development@gmail.com>
2026-05-19 17:17:48 +0200

    refactor: re-order fields according to the mcwiki add to base: glow color override, shadow strength, shadow radius, brightness

--- src/main/java/dev/threeadd/packeteventssk/element/general/field/meta/BaseMetaFieldRegistrar.java
+++ src/main/java/dev/threeadd/packeteventssk/element/general/field/meta/BaseMetaFieldRegistrar.java
@@ -15,15 +15,48 @@ import org.bukkit.entity.Pose;
 
 import java.util.function.Function;
 
+// Should fully match: https://minecraft.wiki/w/Java_Edition_protocol/Entity_metadata#Entity
+// including order of fields
 public class BaseMetaFieldRegistrar implements FieldRegistrar {
 
     @Override
     public boolean register() {
+
+        // complete
         MetaFieldRegistry.Builder<EntityMeta> builder = MetaFieldRegistry.INSTANCE.builder(EntityTypes.ENTITY, EntityMeta.class)
                 .optionalField(Number.class,
                         EntityMeta::getEntityId,
                         null,
                         "entity id", "id")
+                // start of metadata
+                .optionalField(Boolean.class,
+                        EntityMeta::isOnFire,
+                        EntityMeta::setOnFire,
+                        "on fire state", "on fire")
+                .optionalField(Boolean.class,
+                        EntityMeta::isSneaking,
+                        EntityMeta::setSneaking,
+                        "sneaking state", "sneaking")
+                .optionalField(Boolean.class,
+                        EntityMeta::isSprinting,
+                        EntityMeta::setSprinting,
+                        "sprinting state", "sprinting")
+                .optionalField(Boolean.class,
+                        EntityMeta::isSwimming,
+                        EntityMeta::setSwimming,
+                        "swimming state", "swimming")
+                .optionalField(Boolean.class,
+                        EntityMeta::isInvisible,
+                        EntityMeta::setInvisible,
+                        "invisible state", "invisible")
+                .optionalField(Boolean.class,
+                        EntityMeta::isGlowing,
+                        EntityMeta::setGlowing,
+                        "glowing state", "glowing")
+                .optionalField(Boolean.class,
+                        EntityMeta::isFlyingWithElytra,
+                        EntityMeta::setFlyingWithElytra,
+                        "elytra flying state", "elytra flying")
                 .optionalField(Timespan.class,
                         meta -> ConversionUtil.toTimespan(meta.getAirTicks()),
                         (meta, timespan) -> meta.setAirTicks((short) ConversionUtil.toTicks(timespan)),
@@ -35,47 +68,20 @@ public class BaseMetaFieldRegistrar implements FieldRegistrar {
                 .optionalField(Boolean.class,
                         EntityMeta::isCustomNameVisible,
                         EntityMeta::setCustomNameVisible,
-                        "custom name visible", "custom name visible state")
+                        "custom name visible state", "custom name visible")
                 .optionalField(Boolean.class,
-                        EntityMeta::isFlyingWithElytra,
-                        EntityMeta::setFlyingWithElytra,
-                        "elytra flying", "elytra flying state")
+                        EntityMeta::isSilent,
+                        EntityMeta::setSilent,
+                        "silent state", "silent")
                 .optionalField(Boolean.class,
-                        EntityMeta::isOnFire,
-                        EntityMeta::setOnFire,
-                        "on fire", "on fire state")
+                        meta -> !meta.hasNoGravity(),
+                        (meta, newState) -> meta.setHasNoGravity(!newState),
+                        "gravity state", "gravity")
+                // pose (registered conditionally)
                 .optionalField(Timespan.class,
                         meta -> ConversionUtil.toTimespan(meta.getTicksFrozenInPowderedSnow()),
                         (meta, timespan) -> meta.setTicksFrozenInPowderedSnow((short) ConversionUtil.toTicks(timespan)),
                         "frozen time", "frozen ticks")
-                .optionalField(Boolean.class,
-                        EntityMeta::isGlowing,
-                        EntityMeta::setGlowing,
-                        "glowing", "glowing state")
-                .optionalField(Boolean.class,
-                        meta -> !meta.hasNoGravity(),
-                        (meta, newState) -> meta.setHasNoGravity(!newState),
-                        "gravity", "gravity state")
-                .optionalField(Boolean.class,
-                        EntityMeta::isInvisible,
-                        EntityMeta::setInvisible,
-                        "invisible", "invisible state")
-                .optionalField(Boolean.class,
-                        EntityMeta::isSilent,
-                        EntityMeta::setSilent,
-                        "silent", "silent state")
-                .optionalField(Boolean.class,
-                        EntityMeta::isSneaking,
-                        EntityMeta::setSneaking,
-                        "sneaking", "sneaking state")
-                .optionalField(Boolean.class,
-                        EntityMeta::isSprinting,
-                        EntityMeta::setSprinting,
-                        "sprinting", "sprinting state")
-                .optionalField(Boolean.class,
-                        EntityMeta::isSwimming,
-                        EntityMeta::setSwimming,
-                        "swimming", "swimming state")
                 .constructor(BASE_ENTITY_META_CONSTRUCTOR);
 
         // only register if an addon provides this type
@@ -100,6 +106,43 @@ public class BaseMetaFieldRegistrar implements FieldRegistrar {
         }
         EntityMeta meta = EntityMeta.createMeta(entityId.intValue(), context.getKey());
 
+        // start metadata
+
+        Boolean onFire = context.getOptional("on fire state", Boolean.class);
+        if (onFire != null) {
+            meta.setOnFire(onFire);
+        }
+
+        Boolean sneaking = context.getOptional("sneaking state", Boolean.class);
+        if (sneaking != null) {
+            meta.setSneaking(sneaking);
+        }
+
+        Boolean sprinting = context.getOptional("sprinting state", Boolean.class);
+        if (sprinting != null) {
+            meta.setSprinting(sprinting);
+        }
+
+        Boolean swimming = context.getOptional("swimming state", Boolean.class);
+        if (swimming != null) {
+            meta.setSwimming(swimming);
+        }
+
+        Boolean invisible = context.getOptional("invisible state", Boolean.class);
+        if (invisible != null) {
+            meta.setInvisible(invisible);
+        }
+
+        Boolean glowing = context.getOptional("glowing state", Boolean.class);
+        if (glowing != null) {
+            meta.setGlowing(glowing);
+        }
+
+        Boolean elytraFlying = context.getOptional("elytra flying state", Boolean.class);
+        if (elytraFlying != null) {
+            meta.setFlyingWithElytra(elytraFlying);
+        }
+
         Timespan airTime = context.getOptional("air time", Timespan.class);
         if (airTime != null) {
             meta.setAirTicks((short) ConversionUtil.toTicks(airTime));
@@ -115,34 +158,14 @@ public class BaseMetaFieldRegistrar implements FieldRegistrar {
             meta.setCustomNameVisible(customNameVisible);
         }
 
-        Boolean elytraFlying = context.getOptional("elytra flying state", Boolean.class);
-        if (elytraFlying != null) {
-            meta.setFlyingWithElytra(elytraFlying);
+        Boolean silent = context.getOptional("silent state", Boolean.class);
+        if (silent != null) {
+            meta.setSilent(silent);
         }
 
-        Boolean onFire = context.getOptional("on fire", Boolean.class);
-        if (onFire != null) {
-            meta.setOnFire(onFire);
-        }
-
-        Timespan frozenTime = context.getOptional("frozen time", Timespan.class);
-        if (frozenTime != null) {
-            meta.setTicksFrozenInPowderedSnow((short) ConversionUtil.toTicks(frozenTime));
-        }
-
-        Boolean glowing = context.getOptional("glowing", Boolean.class);
-        if (glowing != null) {
-            meta.setGlowing(glowing);
-        }
-
-        Boolean gravity = context.getOptional("gravity", Boolean.class);
+        Boolean gravity = context.getOptional("gravity state", Boolean.class);
         if (gravity != null) {
             meta.setHasNoGravity(!gravity);
-        }
-
-        Boolean invisible = context.getOptional("invisible", Boolean.class);
-        if (invisible != null) {
-            meta.setInvisible(invisible);
         }
 
         Pose pose = context.getOptional("pose", Pose.class);
@@ -150,24 +173,9 @@ public class BaseMetaFieldRegistrar implements FieldRegistrar {
             meta.setPose(SpigotConversionUtil.fromBukkitPose(pose));
         }
 
-        Boolean silent = context.getOptional("silent", Boolean.class);
-        if (silent != null) {
-            meta.setSilent(silent);
-        }
-
-        Boolean sneaking = context.getOptional("sneaking", Boolean.class);
-        if (sneaking != null) {
-            meta.setSneaking(sneaking);
-        }
-
-        Boolean sprinting = context.getOptional("sprinting", Boolean.class);
-        if (sprinting != null) {
-            meta.setSprinting(sprinting);
-        }
-
-        Boolean swimming = context.getOptional("swimming", Boolean.class);
-        if (swimming != null) {
-            meta.setSwimming(swimming);
+        Timespan frozenTime = context.getOptional("frozen time", Timespan.class);
+        if (frozenTime != null) {
+            meta.setTicksFrozenInPowderedSnow((short) ConversionUtil.toTicks(frozenTime));
         }
 
         return meta;
