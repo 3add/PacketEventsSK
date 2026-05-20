6ef382a966f39ccf510f7da5ec9446176b2552f3
3add <3add.development@gmail.com>
2026-05-20 15:10:10 +0200

    bump: project version

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
 }
 
 // Version of PacketEventsSK
-val projectVersion = "1.1.2"
+val projectVersion = "1.1.3"
 // API Version
 val apiVersion = "26.1.2"
 // Minimum paper version that PacketEventsSK supports
